2e9782108c442d627f77ee0483f6d3df61feeb92
mayoi5011 <46701036+mayoi5011@users.noreply.github.com>
2019-04-25 23:02:55 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -25,15 +25,10 @@
 			$url = "https://apecpv.cmru.ac.th/projecterdi/adduser.php?id=".$id;
 			$json = file_get_contents($url);
 			$obj = json_decode($json);
-		
-			foreach($obj as $key=>$value)
-			{
-				$resule = $value;
-			}
 		    
 			$arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
 			$arrayPostData['messages'][0]['type'] = "text";
-			$arrayPostData['messages'][0]['text'] = $resule;
+			$arrayPostData['messages'][0]['text'] = $obj;
 		
 			if($resule == "no"){
 				$arrayPostData['messages'][0]['text'] = "ฉันจำได้ว่าคุณสมัครสมาชิกไปแล้วนะ";
